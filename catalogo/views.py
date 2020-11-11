5aab3d42c1b5c36280f0150df2832f5a7c3946d7
Eduardo Flores <eduardofloresestr@gmail.com>
2020-11-11 12:13:51 -0600

    Borrado individual de productos registrados por el admin

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
@@ -7,7 +7,7 @@ from catalogo.models import Producto
 # Create your views here.
 
 def index(request):
-	productos = Producto.objects.order_by('-nombre')[:5]
+	productos = Producto.objects.order_by('nombre')[:5]
 	template = loader.get_template('catalogo/index.html')
 	context = {
 
@@ -15,3 +15,10 @@ def index(request):
 
 	}
 	return HttpResponse(template.render(context, request))
+
+
+def borrarProducto(request, id):
+	producto = Producto.objects.get(pk=id)
+	if(producto is not None):
+		producto.delete()
+	return redirect('index')
